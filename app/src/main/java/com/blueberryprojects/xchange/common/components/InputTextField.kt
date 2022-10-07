0361856b9e8f3b2cc58f6f50896b83cb8963ab53
Van Javier <vanlaurence08@gmail.com>
2022-10-08 00:35:59 +0800

    upper conversion box UI

---
+++ app/src/main/java/com/blueberryprojects/xchange/common/components/InputTextField.kt
@@ -0,0 +1,70 @@
+package com.blueberryprojects.xchange.common.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun InputTextField(
+    modifier: Modifier,
+    hint: String,
+    onValueChange: (input: String) -> Unit,
+) {
+    var textState by remember {
+        mutableStateOf("")
+    }
+
+    BasicTextField(
+        modifier = modifier,
+        value = textState,
+        textStyle = TextStyle(
+            color = Color.Black
+        ),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Text,
+            autoCorrect = false
+        ),
+        onValueChange = {
+            textState = it
+            onValueChange(textState)
+        },
+        enabled = true,
+        singleLine = true,
+        cursorBrush = SolidColor(Color.Black),
+        decorationBox = { innerTextField ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .weight(0.9f)
+                ) {
+                    if (textState.isEmpty()) {
+                        Text(
+                            modifier = Modifier.alpha(0.5f),
+                            text = hint,
+                            color = Color.Black,
+                        )
+                    }
+                    innerTextField()
+                }
+            }
+        }
+    )
+}
--- app/src/main/java/com/blueberryprojects/xchange/common/components/MainScreen.kt
+++ app/src/main/java/com/blueberryprojects/xchange/common/components/MainScreen.kt
@@ -1,27 +1,135 @@
 package com.blueberryprojects.xchange.common.components
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.blueberryprojects.xchange.R
 import com.blueberryprojects.xchange.featurexchange.presentation.viewmodel.CurrencyViewModel
 
 @Composable
 fun MainScreen(
     currencyViewModel: CurrencyViewModel = hiltViewModel(),
 ) {
-    val rateState = currencyViewModel.rateState.value
+    Column(modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .background(colorResource(id = R.color.dark_navy_blue))
+            .padding(16.dp),
+            contentAlignment = Alignment.Center) {
+            Column {
+                Text(text = "CURRENCY EXCHANGE",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = Color.White,
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center)
+                Spacer(modifier = Modifier.height(24.dp))
+                Row(Modifier
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(Color.White)
+                    .padding(16.dp)) {
+                    Column(verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(60.dp)) {
+                        Text(text = "EUR",
+                            fontSize = 18.sp)
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(text = "1000.00",
+                            fontWeight = FontWeight.SemiBold)
+                    }
+                    Column(verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(60.dp)) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_exchange),
+                            contentDescription = "exchange_icon",
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                    Column(verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(60.dp)) {
+                        Text(text = "USD",
+                            fontSize = 18.sp)
+                        Spacer(modifier = Modifier.height(4.dp))
+                        Text(text = "1000.00",
+                            fontWeight = FontWeight.SemiBold)
+                    }
+                }
+                Spacer(modifier = Modifier.height(18.dp))
+                Button(onClick = {
 
-    Column {
-        rateState.data?.let {
-            Text(text = "${it.result}")
-        }
-
-        Button(onClick = {
-            currencyViewModel.getCurrencyExchangeRate("PHP", "USD", 1.00)
-        }) {
-            Text("Exchange")
+                }, modifier = Modifier
+                    .fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(backgroundColor = Color.White)) {
+                    Text(text = "Convert",
+                        fontSize = 16.sp,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center)
+                }
+            }
         }
     }
+
+
+//    val rateState = currencyViewModel.rateState.value
+//
+//    Column(modifier = Modifier.fillMaxSize()) {
+//
+//        Text(text = "CURRENCY EXCHANGE")
+//        Spacer(modifier = Modifier.height(24.dp))
+//        Row(modifier = Modifier
+//            .fillMaxWidth()) {
+//            InputTextField(modifier = Modifier.weight(1f), hint = "From") { input ->
+//
+//            }
+//            Text(text = "USD",
+//                modifier = Modifier.weight(1f))
+//            Icon(painterResource(id = R.drawable.ic_baseline_arrow_drop_down_24),
+//                contentDescription = "dropdown",
+//                modifier = Modifier.weight(1f))
+//            InputTextField(modifier = Modifier.weight(1f), hint = "From") { input ->
+//
+//            }
+//            Text(text = "PHP",
+//                modifier = Modifier.weight(1f))
+//            Icon(painterResource(id = R.drawable.ic_baseline_arrow_drop_down_24),
+//                contentDescription = "dropdown",
+//                modifier = Modifier.weight(1f))
+//        }
+//    }
+
+//    Column {
+//        rateState.data?.let {
+//            Text(text = "${it.result}")
+//        }
+//
+//        Button(onClick = {
+//            currencyViewModel.getCurrencyExchangeRate("PHP", "USD", 1.00)
+//        }) {
+//            Text("Exchange")
+//        }
+//    }
 }
